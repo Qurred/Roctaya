daea9a9e9eaef86d26f9af2b731b2f17c2760db0
Juho Kuusinen <qurred@gmail.com>
2017-06-18 19:32:13 +0300

    Trying to fix backend

--- server/api.js
+++ server/api.js
@@ -82,7 +82,7 @@ router.post('/signin', (req, res, next) =>{
                 return res.status(500).json({message: `Internal error`});
             }
             console.log(`SELECT * FROM player where UPPER(username) = UPPER(${query[0]})`);
-            const q = client.query('SELECT * FROM player where UPPER(username) = UPPER(($1))', [query[0]]);            
+            const q = client.query("SELECT * FROM player where UPPER(username) = UPPER('($1)')", [query[0]]);            
             q.on('row', (row) =>{
                 //TODO make better
                 if(row.password == hashed){
