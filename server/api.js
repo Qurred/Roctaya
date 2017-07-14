9981360539c53beae4021d979540146c94d5c2d5
Juho Kuusinen <qurred@gmail.com>
2017-07-14 14:10:40 +0300

    API fixing

--- server/api.js
+++ server/api.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const pg = require('pg');
+const pool = new pg.Pool();
 const pgConnectionString = process.env.DATABASE_URL || require('../config.json').url;
 const secret = process.env.SECRET || require('../config.json').secret;;
 const hashSecret = process.env.HASHSECRET || require('../config.json').hash;; //Not the best way, rainbow atk is possible still
 var client = null;
 
-// TODO Create pg pooling
-
+//////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// Make this into multiple modules
+//////////////////////////////////////////////////////////////////////////////
 
 router.get('/news', (req, res, next) => {
   let result = {
@@ -19,7 +21,7 @@ router.get('/news', (req, res, next) => {
   pg.connect(pgConnectionString, (err, client, done) => {
     if (err) {
       done();
-      return res.status(500).json({message: `Internal error`});
+      return res.status(500).json({ message: `Internal error` });
     }
     const q = client.query("SELECT N.title, N.body, N.banner, N.time::timestamp::date, P.nickname  FROM news as N, player as P WHERE N.creator_id = P.id ORDER BY N.id DESC LIMIT 5 OFFSET $1;", [req.query.offset ? req.query.offset : 0]);
     q.on('row', (row) => {
@@ -39,86 +41,47 @@ router.get('/news', (req, res, next) => {
   });
 });
 
-//Signup functions...
-function checkUsername(name){
-
-}
-function checkNickname(name){
-  
-}
-function checkEmail(email){
-  
-}
-function checkPassword(password){
-  
-}
-
-//Currently sending result before querys are done
-
 router.post('/signup', (req, res, next) => {
   if (req.body.query) {
     //username:nickname:email:password
     var query = Buffer.from(req.body.query, `base64`).toString('ascii').split(":");
-    if (query.length != 3) { //TODO change it to 4 when database has email field
+    if (query.length != 4) {
       return res.status(401).json({
         message: 'Invalid signup parameters',
         status: false
       });
     }
-    
     pg.connect(pgConnectionString, (err, client, done) => {
+      let valid = {};
+      function signupCallback(err, result, value) {
+        if (err) {
+          console.log(err);
+        }
+        valid[value] = result.rows.length == 0 ? true : false;
+        if (Object.keys(valid).length == 4){
+          //Checks if there is false values
+          if (!valid.username || !valid.nickname || !valid.email || !valid.password) {
+            return res.status(401).json(valid);
+          }
+          const hashed = crypto.createHmac('sha256', hashSecret).update(password).digest('hex');
+          client.query('INSERT INTO player(username, nickname,email, password) values($1,$2,$3,$4)', [query[0], query[1],query[2], hashed]);
+          done(); //Closes the connection
+          return res.status(200).json(valid);
+        }
+      }
+
       if (err) {
         done();
         console.log(err);
-        return res.status(500).json({message: `Internal error`});
+        return res.status(500).json({ message: `Internal error` });
       }
-
-      let result = {
-        username: true,
-        nickname: true,
-        email: true,
-        password: true
-      };
-
-      //Let's start by checking values
-      //Username
-      console.log("username query");
-      let q = client.query('SELECT * FROM player WHERE UPPER(username) = UPPER($1)', [query[0]]);
-      q.on('row', (row)=>{
-        console.log("username found");
-        result.username = false;
-      })
-      // q.on('end', ()=>{
-      //   console.log('moi');
-      // })
-      //nickname
-      q = client.query("SELECT * FROM player WHERE UPPER(nickname) = UPPER($1)", [query[1]]);
-      q.on('row', (row)=>{
-        result.nickname = false;
-      })
-
-      //Email //TODO uncomment when database has email
-      // q = client.query("SELECT * FROM player where UPPER(email) = UPPER($1)", [query[3]]);
-      // q.on('row', (row)=>{
-      //   result.email = false;
-      // })
-
-      //Password
-      const password = query[2] //TODO change to 3 when email is added
-      if(password.length < 6){
-        result.password = false;
+      client.query('SELECT * FROM player WHERE UPPER(username) = UPPER($1)', [query[0]], (err, row) => signupCallback(err, row, "username"));
+      client.query("SELECT * FROM player WHERE UPPER(nickname) = UPPER($1)", [query[1]], (err, row) => signupCallback(err, row, "nickname"));
+      client.query("SELECT * FROM player where UPPER(email) = UPPER($1)", [query[2]], (err, row) => signupCallback(err, row, "email"));
+      const password = query[3]
+      if (password.length < 6) {//Currently only thing to check if minlength
+        valid.password = false;
       }
-
-      if(!result.username || !result.nickname || !result.email || !result.password){
-        console.log('"sending"');
-        done();
-        return res.status(401).json({result});
-      }
-      console.log('adding');
-      const hashed = crypto.createHmac('sha256', hashSecret).update(password).digest('hex'); //Hashes the password
-      client.query('INSERT INTO player(username, nickname, password) values($1,$2,$3)', [query[0], query[1], hashed]);
-      done(); //Closes the connection
-      return res.status(200).json(result);
     });
   } else {
     return res.status(401).json({
